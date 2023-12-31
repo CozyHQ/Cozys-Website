a767a6dbf01e1795abdc86d639ce072afcca41b4
wave <wave301@icloud.com>
2023-12-31 22:20:55 +0200

    feat(config): added cloudflare adapter and astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  static: {
-    directory: 'public',
-      },
+  integrations: [tailwind(), react(), icon()],
+  output: "server",
+  adapter: cloudflare()
 });
